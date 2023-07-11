8cfa18286b98b68c75cb2087de2220953c72ce0e
SKMNS\202110043 <jeonheejin@sk.com>
2023-07-11 13:38:10 +0900

    commit_20230711

---
+++ src/main/java/hello/aop/exam/annotation/Retry.java
@@ -0,0 +1,12 @@
+package hello.aop.exam.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME) // 런타임시점까지 살아있음
+public @interface Retry {
+    int value() default 3;
+}
---
+++ src/main/java/hello/aop/exam/annotation/Trace.java
@@ -0,0 +1,11 @@
+package hello.aop.exam.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME) // 런타임시점까지 살아있음
+public @interface Trace {
+}
